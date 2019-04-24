66e3e5de8e43aa75abb52c332868466713072686
“yangyangzouzou” <yang.zhou@edu.escpeurope.eu>
2019-04-24 17:51:02 +0200

    login2

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const session = require("express-session");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
 
 
 // Login passport config
