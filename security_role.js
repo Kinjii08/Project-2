0fd1f841c3d67cde14575a9815e89aa409b0845b
Christofer Perez <christoferperez@gmail.com>
2019-04-24 17:18:31 +0200

    security_role

---
+++ security_role.js
@@ -0,0 +1,123 @@
+const checkStudent = checkRoles("student");
+const checkPro = checkRoles("pro");
+const checkUniversity = checkRoles("university");
+
+router.get("/private", checkRoles("student"), (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+router.get("/private", checkRoles("pro"), (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+router.get("/private", checkRoles("university"), (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+router.get("/private", ensureAuthenticated, (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/login");
+  }
+
+  const userModel = Schema({
+    name: String,
+    email: String,
+    desc: String,
+    user: Schema.Types.ObjectId
+  });
+
+  const schoolModel = Schema({
+    name: String,
+    email: String,
+    desc: String,
+    university: Schema.Types.ObjectId
+  });
+
+  const companyModel = Schema({
+    name: String,
+    email: String,
+    desc: String,
+    company: Schema.Types.ObjectId
+  });
+
+  router.post("/student", ensureAuthenticated, (req, res, next) => {
+    const newStudent = new student({
+      name: req.body.name,
+      desc: req.body.desc,
+      student: req.user._id // <-- we add the user ID
+    });
+
+    newStudent.save(err => {
+      if (err) {
+        return next(err);
+      } else {
+        res.redirect("/student");
+      }
+    });
+  });
+
+  router.get("/student", ensureAuthenticated, (req, res, next) => {
+    Room.find({ owner: req.user._id }, (err, myRooms) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.render("student/index", { student: myRooms });
+    });
+  });
+
+  const userModel = mongoose.Schema({
+    /// ...
+    role: {
+      type: String,
+      enum: ["student", "university", "pro"],
+      default: "student"
+    }
+  });
+
+  const schoolModel = mongoose.Schema({
+    /// ...
+    role: {
+      type: String,
+      enum: ["student", "university", "pro"],
+      default: "student"
+    }
+  });
+
+  const companyModel = mongoose.Schema({
+    /// ...
+    role: {
+      type: String,
+      enum: ["student", "university", "pro"],
+      default: "student"
+    }
+  });
+
+  function checkRoles(role) {
+    return function(req, res, next) {
+      if (req.isAuthenticated() && req.user.role === role) {
+        return next();
+      } else {
+        res.redirect("/login");
+      }
+    };
+  }
+
+  router.get("/private", checkStudent, (req, res) => {
+    res.render("private", { user: req.user });
+  });
+
+  router.get("/posts", checkPro, (req, res) => {
+    res.render("private", { user: req.user });
+  });
+
+  router.get("/posts", checkUniversity, (req, res) => {
+    res.render("private", { user: req.user });
+  });
+}
