456a305aab1843d7c968156cc58e6fb33078e2c5
Christofer Perez <christoferperez@gmail.com>
2019-04-24 17:59:43 +0200

    authRouter.js update

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const APIUser = require("./api_user");
-<<<<<<< HEAD
-const APISchool = require("./api_university");
-=======
-const APISchool = require("./api_school");
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const bcryptSalt = 10;
-const passport = require("passport");
-const ensureLogin = require("connect-ensure-login");
->>>>>>> 377eac7c4436f587d3fdd4ada34317b3b2c7bfcb
+const APIuniversity = require("./api_university");
 
-
-//Passport Login 
+//Passport Login
 router.get("/login", (req, res, next) => {
   res.render("login.hbs");
 });
@@ -22,50 +12,52 @@ authRoutes.get("/login", (req, res, next) => {
   res.render("/login");
 });
 
-authRoutes.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+authRoutes.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 // post te permet de récupérer les infos du formulaire de signup
 router.post("/signup", (req, res) => {
-const email = req.body.email;
-const password = req.body.password;
+  const email = req.body.email;
+  const password = req.body.password;
 
-if (username === "" || password === "") {
-  res.render("/signup", { message: "Indicate username and password" });
-  return;
-}
-
-User.findOne({ username })
-.then(user => {
-  if (user !== null) {
-    res.render("/signup", { message: "The username already exists" });
+  if (username === "" || password === "") {
+    res.render("/signup", { message: "Indicate username and password" });
     return;
   }
 
-  const salt = bcrypt.genSaltSync(bcryptSalt);
-  const hashPass = bcrypt.hashSync(password, salt);
+  User.findOne({ username })
+    .then(user => {
+      if (user !== null) {
+        res.render("/signup", { message: "The username already exists" });
+        return;
+      }
 
-  const newUser = new User({
-    email,
-    password: hashPass
-  });
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
 
-  newUser.save((err) => {
-    if (err) {
-      res.render("/signup", { message: "Something went wrong" });
-    } else {
-      res.redirect("/");
-    }
-  });
-})
-.catch(error => {
-  next(error)
-})
+      const newUser = new User({
+        email,
+        password: hashPass
+      });
 
+      newUser.save(err => {
+        if (err) {
+          res.render("/signup", { message: "Something went wrong" });
+        } else {
+          res.redirect("/");
+        }
+      });
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 // get te permet de servir les pages
@@ -94,13 +86,13 @@ router.post("/user_profile/edit/:id", (req, res) => {
 });
 
 router.get("/university-profile/:id", (req, res) => {
-  APISchool.getOne(req.params.id)
+  APIuniversity.getOne(req.params.id)
     .then(school => res.render("university_profile", { school }))
     .catch(dberr => res.send(dberr));
 });
 
 router.get("/university_profile/edit/:id", (req, res) => {
-  APISchool.getOne(req.params.id)
+  APIuniversity.getOne(req.params.id)
     .then(school => res.render("university_edit", { school }))
     .catch(dberr => res.send(dberr));
 });
@@ -108,7 +100,7 @@ router.get("/university_profile/edit/:id", (req, res) => {
 router.post("/university_profile/edit/:id", (req, res) => {
   console.log(req.params.id);
   console.log(req.body); //body contient les data du formulaires school
-  APISchool.updateOne(req.params.id, req.body)
+  APIuniversity.updateOne(req.params.id, req.body)
     .then(() => res.redirect("/university_profile/edit/" + req.params.id))
     .then(err => res.redirect("/university_profile/edit/"));
 });
