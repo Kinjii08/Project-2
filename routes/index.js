2a41372f30e0ef9b25e0913294e663ff4c579eda
“yangyangzouzou” <yang.zhou@edu.escpeurope.eu>
2019-04-26 00:52:50 +0200

    university pull

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,12 @@ router.get("/contact", (req, res, next) => {
   res.render("contact.hbs");
 });
 
+
+
+
+
+router.get("/university_profile", (req, res, next) => {
+  res.render("university_list.hbs");
+});
+
 module.exports = router;
